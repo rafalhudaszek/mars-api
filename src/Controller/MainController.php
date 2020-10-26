3696ab3529ea0dee1aa69309b804351e1e758dcd
rafalhudaszek <rafalhudaszek98@gmail.com>
2020-10-26 11:57:49 +0100

    backend upgrade

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,18 +5,22 @@ namespace App\Controller;
 use App\Entity\Measurement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpClient\HttpClient;
 
 class MainController extends AbstractController
 {
+
     /**
-     * @Route("/{id?}", name="main")
+     * @Route("/{id?}", name="init")
      * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
      */
-    public function index(Request $request)
+    public function init(Request $request)
     {
+        $session = $this->get("session");
+
         $httpClient = HttpClient::create();
         $response = $httpClient->request('GET', 'https://api.nasa.gov/insight_weather/?api_key=zpj8nG5YUcakf6K1RihjAd75zX8QJmgceeqx2B4f&feedtype=json&ver=1.0');
         $statusCode = $response->getStatusCode();
@@ -48,11 +52,10 @@ class MainController extends AbstractController
                 $measurement->setPavg($table[$measurement->getId()]['PRE']['av']);
 
                 $measurement->setWind($table[$measurement->getId()]['HWS']['av']);
-
-                //$measurement->setCompass($table[$measurement->getId()]['WD']);
-
             }
-            //$id = ($request->get('id') - $measurementTable[count($measurementTable)-1]->getId()) * (-1);
+
+            $session->set('table', $measurementTable);
+
             $id = $request->get('id') - $measurementTable[0]->getId();
             $renderFlag = true;
             if($id > count($table['sol_keys']) || $id < 0)
@@ -62,9 +65,33 @@ class MainController extends AbstractController
             }
             return $this->render('Main/index.html.twig',[
                 'table' => $measurementTable,
-                'id' => $id,
-                'renderFlag' => $renderFlag
+
             ]);
         }
     }
+
+    /**
+     * @Route("/pomiar/{id?}", name="pomiar")
+     * @param Request $request
+     * @return Response
+     */
+    public function display(Request $request)
+    {
+        $session = $this->get('session');
+        $measurementTable = $session->get('table');
+        $id = $request->get('id') - $measurementTable[0]->getId();
+        $renderFlag = true;
+        if($id > count($measurementTable) || $id < 0)
+        {
+            $id = 5;
+            $renderFlag = false;
+        }
+        return $this->render('Main/display.html.twig',[
+            'table' => $measurementTable,
+            'id' => $id,
+            'renderFlag' => $renderFlag
+        ]);
+    }
+
+
 }
